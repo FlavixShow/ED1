7276374f92292203408ed49982ce8a87a6672a21
FlavixShow <flavixmesq@gmail.com>
2025-10-13 18:31:32 -0300

    ex01 - Quantas vezes um número repete

--- Matéria/Array/ex01.c
+++ Matéria/Array/ex01.c
@@ -12,18 +12,18 @@ int main(){
 	printf("Digite um valor X: ");
 	scanf("%i", &x);
 	
-	// tá fora para todos os valores de X usarem os mesmos valores
+	// está fora para que todos os valores de X usarem os mesmos valores
 	for(int i = 0; i < 100; i++)
 		vet[i] = 1 + rand() % 19;
 		
 	for(int j = x; j > 0; j--){
 		for(int i = 0; i < 100; i++){
 			if(vet[i] == j){
-				//printf("%i*\n", vet[i]);
+				printf("%i*\n", vet[i]);
 				cont++;
 				continue;
 			}
-			//printf("%i\n", vet[i]);
+			printf("%i\n", vet[i]);
 		}
 		printf("\nO número %i apareceu %i vezes.\n\n", x--, cont);
 		cont = 0;
