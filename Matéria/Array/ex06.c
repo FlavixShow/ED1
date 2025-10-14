07cdbb8b0cf6ef31c2a4bd36f6e779dc3c8f1930
FlavixShow <flavixmesq@gmail.com>
2025-10-14 12:32:05 -0300

    ex06 - Imprimir matriz N x N com a diagonal principal sendo a soma da linha e coluna

---
+++ Matéria/Array/ex06.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+	
+	int n, soma = 0;
+	
+	printf("Insira o valor de N: ");
+	scanf("%i", &n);
+	
+	int matriz[n][n] = {};
+	
+	srand(time(NULL));
+	
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(i == j){
+				matriz[i][j] = 0;
+				continue;
+			}
+			matriz[i][j] = 1 + rand() % 5;
+		}
+	}
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			if(i == j){
+				for(int k = 0; k < n; k++)
+					soma += matriz[i][k] + matriz[k][j];
+					
+				matriz[i][j] = soma;
+				soma = 0;
+			}
+		printf("%02i\t", matriz[i][j]);
+		}
+		printf("\n\n");
+	}
+	
+
+
+}
