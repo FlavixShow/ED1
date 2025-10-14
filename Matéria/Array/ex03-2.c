da035c40e3c907d86dcad8886bdfe80049d996c8
FlavixShow <flavixmesq@gmail.com>
2025-10-14 15:26:40 -0300

    Correções

--- Matéria/Array/ex03-2.c
+++ Matéria/Array/ex03-2.c
@@ -3,14 +3,19 @@
 #include <time.h>
 
 int main(){
+	//O jeito certo//
 	
-	int vet[40];
-	int tamVet = 40;
+	int tamVet;
+	do{
+		printf("Insira o tamanho do vetor: ");
+		scanf("%i", &tamVet);
+	}while(tamVet%4);
+	
+	int vet[tamVet];
 	
 	//define os valores e imprime o vetor normal;
 	for(int i = 0; i < tamVet; i++){
 		vet[i] = 1 + rand() % 20;
-		//vet[i] = i; //para facilitar o teste
 		printf("%02i ", vet[i]);
 		if(!((i+1)%10))
 			printf("| ");
@@ -19,9 +24,9 @@ int main(){
 	
 	//imprime o vetor com as metades trocadas;
 	for(int i = 0; i < tamVet/2; i++){
-		vet[i] += vet[i + tamVet/2];
-		vet[i + tamVet/2] = vet[i] - vet[i + tamVet/2];
-		vet[i] -= vet[i + tamVet/2];
+		int troca = vet[i];
+		vet[i] = vet[i + tamVet/2];
+		vet[i + tamVet/2] = troca;
 	}
 	for(int i = 0; i < tamVet; i++){
 		printf("%02i ", vet[i]);
@@ -31,12 +36,11 @@ int main(){
 	printf("\n\n");
 	
 	//imprime o vetor com as metades das metades trocadas;
-	int pulaMeio = tamVet/2;
 	for(int i = 0; i < 2; i++){
 		for(int j = 0; j < tamVet/4; j++){
-			vet[j+pulaMeio*i] += vet[(j+pulaMeio*i) + tamVet/4];
-			vet[(j+pulaMeio*i) + tamVet/4] = vet[j+pulaMeio*i] - vet[(j+pulaMeio*i) + tamVet/4];
-			vet[j+pulaMeio*i] -= vet[(j+pulaMeio*i) + tamVet/4];
+			int troca = vet[j+tamVet/2*i];
+			vet[j+tamVet/2*i] = vet[j+tamVet/2*i + tamVet/4];
+			vet[(j+20*i) + tamVet/4] = troca;
 		}
 	}
 	for(int i = 0; i < tamVet; i++){
