be6ab7d1675e75f79878b7ecd32f7e9834def8e8
FlavixShow <flavixmesq@gmail.com>
2025-10-14 14:32:44 -0300

    ex02 - correção

--- Matéria/Array/ex02.c
+++ Matéria/Array/ex02.c
@@ -5,14 +5,17 @@
 int main(){
 	
 	int faces[6];
-	int n;
+	int n, num;
 	
 	printf("Quantas vezes o dado será jogado? ");
 	scanf("%i", &n);
 	
 	srand(time(NULL));
 	for(int i = 0; i < n; i++){
-		switch (1 + rand() % 6){
+		num = 1 + rand() % 6;
+		faces[num - 1]++;
+		
+		/*switch (1 + rand() % 6){
 			case 1: faces[0]++;
 			break;
 			case 2: faces[1]++;
@@ -25,7 +28,7 @@ int main(){
 			break;
 			case 6: faces[5]++;
 			break;
-		}
+		}*/
 	}
 	for(int i = 0; i < 6; i++){
 		printf("O lado %i caiu %i vezes -> %.2f%c\n", i+1, faces[i], (float)faces[i]/n*100, '%');
